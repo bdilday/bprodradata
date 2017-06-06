3d2c3da513caca86c34b575489fc021f6efadfe2
Ben Dilday <ben.dilday.phd@gmail.com>
2017-06-06 11:15:47 -0400

    load runs

--- R/load_dra_runs.R
+++ R/load_dra_runs.R
@@ -1,4 +1,4 @@
 #' @export
 load_dra_runs <- function(year) {
-  readRDS(sprintf("%s/bprodradata/extdata/dra_runs_%d.rds", .libPaths()[[1]], year))
+  readRDS(sprintf("%s/bprodradata/extdata/dra_results_%d.rds", .libPaths()[[1]], year))
 }
